1a22e7a3c9bde20773f0e77d4aa8285c90d0efff
Grokir <grokir13@yandex.ru>
2026-07-03 00:40:38 +0300

    UPD: add global variable for DB-file names

--- source/db_agent/config.py
+++ source/db_agent/config.py
@@ -1,6 +1,8 @@
 MODEL_NAME = "hf.co/yandex/YandexGPT-5-Lite-8B-instruct-GGUF:Q4_K_M"
 
 PATH_DB_DIR = "./db_files"
+EMPLOYEES_TB = "employees.csv"
+CLIENTS_TB = "clients.csv"
 
 SYSPROMPT = """Ты AI-агент, который взаимодействует с базой данных.
 БД представлена в виде двух CSV-файлой: clients.csv и employees.csv"""
