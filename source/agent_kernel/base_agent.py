e3d169bdf5454ed459564fe3ef07d7d7fe35f29f
Grokir <grokir13@yandex.ru>
2026-07-02 20:58:54 +0300

    ADD: base agent core

---
+++ source/agent_kernel/base_agent.py
@@ -0,0 +1,44 @@
+import subprocess
+import requests
+import os
+from typing import Optional
+
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain.agents import create_agent
+from langgraph.checkpoint.memory import MemorySaver
+
+OLLAMA_URL = "http://localhost:11434/api/chat"
+
+CONFIG = {"configurable": {"thread_id": "session-1"}}
+AGENT_EXEC = None
+
+def kernel_init(model_name:str, tools:list, sysprompt:str):
+    global OLLAMA_URL
+
+    llm = ChatOpenAI(
+        model=model_name,
+        base_url=OLLAMA_URL,
+        api_key="not-needed",
+        # temperature=0.1,
+        temperature=0,
+    )
+
+    return create_agent(
+        llm,
+        tools,
+        system_prompt=sysprompt,
+        checkpointer=MemorySaver(),
+    )
+
+async def send_prompt(agent, input_str: str, role:str="user"):
+    return await agent.ainvoke(
+        {"role": role, "messages": [HumanMessage(content=input_str)]},
+        config=CONFIG
+    )
+
+def memory_clear(agent):
+    global CONFIG
+    agent.update_state(CONFIG, {"messages": []}) 
+    
