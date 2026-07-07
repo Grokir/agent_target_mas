27aa78d5188ad44d77921d3f8d542127017163a0
Grokir <grokir13@yandex.ru>
2026-07-07 23:05:35 +0300

    UPD: was add tests for agent methods

--- source/main.py
+++ source/main.py
@@ -1,9 +1,16 @@
-import requests
-# import json
-import time
-import random
+
+# Отключение предупреждений от Pydantic об версии Python
+import warnings
+
+# Задаем жесткое "глушение" для всего
+warnings.filterwarnings("ignore")
+# "Ломаем" функции, которыми langchain пытается сбросить или изменить фильтры
+warnings.resetwarnings = lambda: None
+warnings.filterwarnings = lambda *args, **kwargs: None
+
 
 import agent_repeater_server
+from tests.runner import run_tests
 
 
 LHOST = "localhost"
@@ -12,7 +19,8 @@ LPORT = 5000
 
 def main():
     # запускаем репитер
-    agent_repeater_server.run(LHOST, LPORT)
+    # agent_repeater_server.run(LHOST, LPORT)
+    run_tests()
 
 
 if __name__ == "__main__":
