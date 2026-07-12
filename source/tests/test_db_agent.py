cb16673381d1b73288a86c34bc5482c04c30ee96
Grokir <grokir13@yandex.ru>
2026-07-12 17:47:37 +0300

    UPD: fix agent methods for work with DB

--- source/tests/test_db_agent.py
+++ source/tests/test_db_agent.py
@@ -19,7 +19,8 @@ async def test_base_work() -> tuple[str, bool, str]:
         Должность: Project Manager,
         Отдел: IT-dep,
         Заработная плата: 30_000""",
-        "Удали сотрудника с логином roma_228"
+        "Удали сотрудника с логином roma_228",
+        "У сотрудника с логином natalia_s нужно изменить фамилию на Проскурина, оставив все остальны поля без изменения."
 
     ]
     queue_prompts_clients = [
@@ -33,7 +34,8 @@ async def test_base_work() -> tuple[str, bool, str]:
         contract_start: 2030-03-30,
         status: Planning,
         account_manager: Волков А.А.""",
-        "Удали клиента с ID 13."
+        "Удали клиента с ID 13.",
+        "Нужно обновить данные для клиента с ID 12: изменить менеджера (Account_manager) на Евгению Фокину."
     ]
     try:
         agent = DB_Agent()
@@ -44,9 +46,11 @@ async def test_base_work() -> tuple[str, bool, str]:
     try:
         # print("\t\t[employee]")
         for p in queue_prompts_employee:
-            # await agent.send_message(p)
-            print((await agent.send_message(p))["messages"][-1].content)
+            await agent.send_message(p)
+            # print((await agent.send_message(p))["messages"][-1].content)
             # print((await agent.send_message(p)))
+            # print()
+
     except Exception as e:
         test_res_flag = False
         test_res_comment += ' ' + str(e)
@@ -55,9 +59,10 @@ async def test_base_work() -> tuple[str, bool, str]:
     try:
         # print("\t\t[clients]")
         for p in queue_prompts_clients:
-            # await agent.send_message(p)
-            print((await agent.send_message(p))["messages"][-1].content)
+            await agent.send_message(p)
+            # print((await agent.send_message(p))["messages"][-1].content)
             # print((await agent.send_message(p)))
+            # print()
     except Exception as e:
         test_res_flag = False
         test_res_comment += ' ' + str(e)
